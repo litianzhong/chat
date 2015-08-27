092fc632a90aa7dd67881b8b5f3859d5130556e1
litianzhong <litianzhong@qq.com>
2015-08-27 21:11:51 +0800

    init

---
+++ web/controller/homeController.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * 登录的处理
+ * @author      李天中
+ * @version     1.0
+ */
+class homeController extends Controller {
+        public function __construct() {
+                parent::__construct();
+        }
+        
+		/**登录页*/
+        public function index() {
+			$this->display("login.html");
+        }
+        
+        /****
+         * validate ajax
+         */
+        public function check(){
+			
+        	header('Content-Type: application/json; charset=utf-8');
+			parent::$LogUtil->log(print_r($_POST,true));
+        	$model=$this->model("home");//获得model
+        	$user=json_decode($_POST["model"]);
+        	$info=$model->checkUser($user);
+        	$msg=array();
+        	if($info["flag"]){
+        		$msg = array(
+        				"code" => "1",
+        				"tip" => "welcome!".$info['name'],
+        		);//成功提示信息
+        	}else{
+        		$msg = array(
+        				"code" => "0",
+        				"tip" => "username or password is wrong!",
+        		);//成功提示信息
+        	}
+        	echo json_encode($msg);
+        }
+        /***
+         * login ajax
+         */
+        public function login(){
+        	parent::$LogUtil->log(print_r($_POST,true));
+        	$model=$this->model("home");//获得model
+        	$user=json_decode($_POST["model"]);
+        	$info=$model->checkUser($user);
+        	$msg=array();
+        	if($info["flag"]){
+        		Application::$_lib["SessionAuth"]->set("_USER", $user->username);//校验成功放入session
+        		$this->display("login.html");
+        	}else{
+        		$this->display("login.html");
+        	}
+        	
+        }
+}
+
---
+++ web/model/homeModel.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * 
+ * 登录控制页
+ * @author      李天中
+ * @version     1.0
+ */
+class homeModel extends Model {
+	/**
+	 * 读取文件校验用户是否正确
+	 * 校验用户是否正确
+	 * 
+	 * @param $user 用户
+	 */
+	public function checkUser($user) {
+		
+		$sql="select name from member where id=? and password=?";
+		$flag=false;
+		$name="";
+		$result=$this->db->valueQuery($sql,array($user->username,$user->userpwd));
+		if($result!=null){
+			$flag=true;
+		}
+		return array("flag"=>$flag,"name"=>$result);
+	}
+}
